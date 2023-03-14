ec794fec936ea9f7853854bb31f331b22ee758a4
Masnan <zhinanwang@znbuchadoudembp.wifi.local.cmu.edu>
2023-03-14 15:49:54 -0400

    query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,5 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("What is your name")) {
+  if (query.includes("What is your name?")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
