0d8cc69d1a050e8fed0a7e12234f2a4bf01f4f9a
Masnan <zhinanwang@znbuchadoudembp.wifi.local.cmu.edu>
2023-03-14 15:57:59 -0400

    query 3

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -10,17 +10,17 @@ export default function QueryProcessor(query: string): string {
       "79"
     );
   }
-  else if (query.toLowerCase().includes("What is 7 plus 36?")) {
+  else if (query.toLowerCase().includes("what is 7 plus 36?")) {
     return (
       "43"
     );
   }
-  else if (query.toLowerCase().includes("What is 54 plus 94?")) {
+  else if (query.toLowerCase().includes("what is 54 plus 94?")) {
     return (
       "148"
     );
   }
-  else if (query.toLowerCase().includes("Which of the following numbers is the largest: 39, 99, 61?")) {
+  else if (query.toLowerCase().includes("which of the following numbers is the largest: 39, 99, 61?")) {
     return (
       "99"
     );
