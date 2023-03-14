4a79b78ef116267e32e316ab912807036e96dee4
Masnan <zhinanwang@znbuchadoudembp.wifi.local.cmu.edu>
2023-03-14 15:54:40 -0400

    query 3

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,6 +1,6 @@
 export default function QueryProcessor(query: string): string {
   console.log(query);
-  if (query.toLowerCase().includes("What is your name?")) {
+  if (query.toLowerCase().includes("what is your name?")) {
     return (
       "TeSam"
     );
